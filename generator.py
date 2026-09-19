11f929b5d8451c08aded30e0b22aa1506de76775
Arthur <arthxrmarques@gmail.com>
2026-09-19 00:10:13 -0300

    feat:cria o labirinto em generator obs:to fazendo a parte de exibir o labirinto

--- generator.py
+++ generator.py
@@ -9,26 +9,101 @@ class Cell:
         self.walls = {"N": True, "E": True, "S": True, "W": True}
         self.visited = False
 
-    def open_path(current : str, next : str) -> None:
-        if y == "W":
-            self.walls = {"N": True, "E": True, "S": True, "W": False}
+
+    def open_path(self, next : "Cell") -> None:
+        if next.x - self.x > 0 :
+            self.walls["E"] = False 
+            next.walls["W"] =  False
+        if next.y - self.y > 0:
+            self.walls["S"] = False
+            next.walls["N"] = False
+        if next.x - self.x == -1:
+            self.walls["W"] = False 
+            next.walls["E"] =  False
+        if next.y - self.y == -1:
+            self.walls["N"] = False 
+            next.walls["S"] =  False
 
 
 class Generator:
 
     def __init__(self) -> None:
         self.seed = None
+        self.width = None
+        self.height = None
         with open("config.txt","r") as file:
             for line in file:
+                if line.startswith("WIDTH="):
+                    self.width = int(line.split("=")[1])
+                if line.startswith("HEIGHT="):
+                    self.height = int(line.split("=")[1])
                 if line.startswith("SEED="):
-                    seed = int(line.split("=")[1])
-                    break
-        if seed is None:
-            seed = random.randint(0 ,2**32 - 1)    
+                    self.seed = int(line.split("=")[1])
+        if self.width <= 0 or self.height <= 0:
+            raise ValueError("Tamanho inválido")
+        if self.seed is None:
+            self.seed = random.randint(0 ,2**32 - 1)
+        
+
+    def generate_maze(self) ->None:
+        random.seed(self.seed)
+        self.grid : [list[list[Cell]]]
+        self.grid = []
+        for y in range(self.height):
+            line = []
+            for x in range(self.width):
+                cell = Cell(x,y)
+                line.append(cell)
+            self.grid.append(line)            
+        self.listcel = [self.grid[0][0]] 
+        while(self.listcel):
+            neighbours = self.get_neighbours(self.listcel[-1])
+            if not neighbours :
+                self.listcel.pop()
+            else:
+                next = random.choice(neighbours)
+                self.listcel[-1].open_path(next)
+                next.visited = True
+                self.listcel.append(next)
 
 
+    def get_neighbours(self, current : Cell) -> list[Cell]:
+            neighbourslist = []
+            deslocations = [(0,-1), (0,1), (-1,0), (1,0)]
+            for dx, dy in deslocations:
+                nx = current.x + dx
+                ny = current.y + dy
+                if 0 <= nx < self.width and 0 <= ny < self.height:
+                    neighbour = self.grid[ny][nx]
+                    if neighbour.visited == False:
+                        neighbourslist.append(neighbour)
 
-    def generate_maze() ->None:
-        seed = random.seed(self.seed)
+            return neighbourslist
 
-    
+    def show_maze(self) -> None:
+        self.grid
+
+        for y in self.grid:
+            top_line = ""
+            mid_line = ""
+            for x range(self.grid[y][x-1]):
+                cell = self.grid[y][x]
+                if cell.walls["N"] == True:
+                    if x == 1 or x == x-1
+                        top_line += "+"
+                    top_line += "---"
+                
+                if cell.walls["W"] == True:
+                    mid_line += "|"
+                print(top_line)
+                print(mid_line )  
+
+
+if __name__ == "__main__":
+    gen = Generator()
+    gen.generate_maze()
+
+    for y in range(gen.height):
+        for x in range(gen.width):
+            cell = gen.grid[y][x]
+            print(f"({x},{y}) -> {cell.walls}")
